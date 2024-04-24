a1c34fab18f47ff07b242274fd5d07f328f743a1
marcely <arrudamarcely460@gmail.com>
2024-04-24 10:39:05 -0400

    add questao1.js

---
+++ questao1.js
@@ -0,0 +1,8 @@
+/*
+Questão 1: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é par ou ímpar. A variável numero contém o valor a 
+ser verificado. Atribua o resultado à variável resultado.
+*/
+let numero =2;
+let resultado =( numero % 2 == 0  ) ? "par" : "impar";
+console.log(resultado);
